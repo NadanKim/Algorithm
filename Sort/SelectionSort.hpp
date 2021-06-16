d5700ab7b3495ea7ef15cadbe44dd9854380eaa4
김나단 <paintime@naver.com>
2021-06-16 11:05:45 +0900

    알고리즘 솔루션 추가

---
+++ Sort/SelectionSort.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void SelectionSort(int arr[], int n)
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿#include "Sort/SelectionSort.hpp"
+
+int main()
+{
+	int arr[10] = { 2, 4, 6, 8, 10, 9, 7, 5, 3, 1 };
+
+	SelectionSort(arr, 10);
+}
