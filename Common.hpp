f69f71284ebdb4deaf946d756380a406964207d1
김나단 <paintime@naver.com>
2021-06-23 08:26:03 +0900

    주석 추가

--- Common.hpp
+++ Common.hpp
@@ -13,6 +13,11 @@
 namespace Common
 {
 #ifdef MAKE_RANDOM_NUMBERS
+	/// <summary>
+	/// 난수 배열을 생성하여 반환한다.
+	/// </summary>
+	/// <param name="n">생성할 난수의 개수</param>
+	/// <returns>생성된 난수 배열</returns>
 	int* GenerateRandomNumber(int n = 100'000)
 	{
 		std::default_random_engine randomEngine;
@@ -27,6 +32,10 @@ namespace Common
 		return arr;
 	}
 
+	/// <summary>
+	/// 난수를 생성하여 파일에 저장한다.
+	/// </summary>
+	/// <param name="n">생성할 난수의 개수</param>
 	void MakeRandomNumbersFile(int n = 100'000)
 	{
 		int* arr = GenerateRandomNumber(n);
@@ -44,6 +53,11 @@ namespace Common
 #endif
 
 #pragma region 파일 입출력
+	/// <summary>
+	/// 파일에 있는 난수를 불러와 입력된 포인터에 저장한다.
+	/// </summary>
+	/// <param name="arr">불러온 난수 배열을 반환할 포인터</param>
+	/// <returns>불러온 난수의 개수</returns>
 	int LoadRandomNumbersInFile(int** arr)
 	{
 		std::ifstream ifs("./Randoms.txt");
@@ -67,16 +81,25 @@ namespace Common
 	std::clock_t startTime;
 	std::clock_t endTime;
 
+	/// <summary>
+	/// 시간 측정을 시작한다.
+	/// </summary>
 	void StartClock()
 	{
 		startTime = std::clock();
 	}
 
+	/// <summary>
+	/// 시간 측정을 종료한다.
+	/// </summary>
 	void StopClock()
 	{
 		endTime = std::clock();
 	}
 
+	/// <summary>
+	/// 측정한 시간을 계산하여 출력한다.
+	/// </summary>
 	void PrintElapsedTime()
 	{
 		std::clock_t elapsed = endTime - startTime;
@@ -86,8 +109,11 @@ namespace Common
 		std::cout << "total time is " << timeSecond << "s ( " << elapsed << "ms )\n";
 	}
 #pragma endregion
-
-
+	/// <summary>
+	/// 입력된 배열을 출력한다.
+	/// </summary>
+	/// <param name="arr">출력할 배열</param>
+	/// <param name="n">배열의 길이</param>
 	void PrintArray(int arr[], int n)
 	{
 		for (int i = 0; i < n; i++)
@@ -97,6 +123,12 @@ namespace Common
 		std::cout << '\n';
 	}
 
+	/// <summary>
+	/// 주어진 인덱스의 값을 서로 바꾼다.
+	/// </summary>
+	/// <param name="arr">값이 들어있는 배열</param>
+	/// <param name="a">첫 번째 인덱스</param>
+	/// <param name="b">두 번째 인덱스</param>
 	void Swap(int arr[], int a, int b)
 	{
 		int temp{ arr[a] };
--- Sort/SelectionSort.hpp
+++ Sort/SelectionSort.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include "../Common.hpp"
 
+/// <summary>
+/// 주어진 배열을 정렬한다.
+/// </summary>
+/// <param name="arr">정렬을 진행할 배열</param>
+/// <param name="n">배열의 길이</param>
+/// <param name="printData">중간 결과 출력 여부</param>
 void SelectionSort(int arr[], int n, bool printData = false)
 {
 	if (printData)
