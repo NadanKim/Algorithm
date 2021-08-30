cde93af74cd98bb6f9e3f3c939a2817e6e800237
김나단 <paintime@naver.com>
2021-08-31 01:28:55 +0900

    AVLTree 구현 완료

--- 검색트리/AVLTree.cpp
+++ 검색트리/AVLTree.cpp
@@ -107,7 +107,8 @@ void AVLTree::Insert(int data)
 		return;
 	}
 
-	AVLNode* node{ Insert(_root, data) };
+	Insert(_root, data);
+	CalculateBalaceFactor(_root);
 }
 
 /// <summary>
@@ -121,7 +122,8 @@ void AVLTree::Delete(int data)
 		return;
 	}
 
-	AVLNode* node{ Delete(GetNode(data)) };
+	Delete(GetNode(data));
+	CalculateBalaceFactor(_root);
 }
 
 /// <summary>
@@ -129,15 +131,13 @@ void AVLTree::Delete(int data)
 /// </summary>
 /// <param name="parent">삽입해야 할 노드의 부모</param>
 /// <param name="data">삽입할 값</param>
-AVLNode* AVLTree::Insert(AVLNode* parent, int data)
+void AVLTree::Insert(AVLNode* parent, int data)
 {
 	if (parent == nullptr)
 	{
 		AVLNode* node{ _nodeManager.Pop() };
 		node->data = data;
 		_root = node;
-
-		return node;
 	}
 	else if (parent->data > data)
 	{
@@ -147,12 +147,10 @@ AVLNode* AVLTree::Insert(AVLNode* parent, int data)
 			node->data = data;
 			node->parent = parent;
 			parent->left = node;
-
-			return node;
 		}
 		else
 		{
-			return Insert(parent->left, data);
+			Insert(parent->left, data);
 		}
 	}
 	else
@@ -163,12 +161,10 @@ AVLNode* AVLTree::Insert(AVLNode* parent, int data)
 			node->data = data;
 			node->parent = parent;
 			parent->right = node;
-			
-			return node;
 		}
 		else
 		{
-			return Insert(parent->right, data);
+			Insert(parent->right, data);
 		}
 	}
 }
@@ -177,7 +173,7 @@ AVLNode* AVLTree::Insert(AVLNode* parent, int data)
 /// AVL 트리 제거 처리
 /// </summary>
 /// <param name="node">제거할 노드</param>
-AVLNode* AVLTree::Delete(AVLNode* node)
+void AVLTree::Delete(AVLNode* node)
 {
 	AVLNode* parent{ node->parent };
 
@@ -233,8 +229,6 @@ AVLNode* AVLTree::Delete(AVLNode* node)
 		node->data = child->data;
 		Delete(child);
 	}
-
-	return parent;
 }
 
 /// <summary>
@@ -283,5 +277,165 @@ bool AVLTree::IsLeftNode(AVLNode* node)
 bool AVLTree::IsRightNode(AVLNode* node)
 {
 	return node->parent != nullptr && node->parent->right == node;
+}
+
+/// <summary>
+/// 주어진 노드를 기준으로 왼쪽으로 회전한다.
+/// </summary>
+/// <param name="node">기준 노드</param>
+AVLNode* AVLTree::RotateLeft(AVLNode* node)
+{
+	AVLNode* x{ node };
+	AVLNode* c{ x->right };
+
+	c->left = x;
+	c->parent = x->parent;
+	x->parent = c;
+	x->right = nullptr;
+
+	return c;
+}
+
+/// <summary>
+/// 주어진 노드를 기준으로 오른쪽으로 회전한다.
+/// </summary>
+/// <param name="node">기준 노드</param>
+AVLNode* AVLTree::RotateRight(AVLNode* node)
+{
+	AVLNode* x{ node };
+	AVLNode* c{ x->left };
+
+	c->right = x;
+	c->parent = x->parent;
+	x->parent = c;
+	x->left = nullptr;
+
+	return c;
+}
+
+/// <summary>
+/// 주어진 노드의 BF를 구하여 반환한다.
+/// </summary>
+/// <param name="node">타겟 노드</param>
+int AVLTree::CalculateBalaceFactor(AVLNode* node)
+{
+	if (node == nullptr)
+	{
+		return 0;
+	}
+
+	CalculateBalaceFactor(node->left);
+	CalculateBalaceFactor(node->right);
+
+	node->bf = GetNodeDepth(node->left) - GetNodeDepth(node->right);
+
+	// 밸런스가 무너진 경우
+	if (node->HasBalanceProblem())
+	{
+		AVLNode* x{ node };
+		AVLNode* c{ x->left != nullptr ? x->left : x->right };
+		AVLNode* c2{ c->left != nullptr ? c->left : c->right };
+
+		if (IsLLCase(c, c2))
+		{
+			node = RotateRight(x);
+		}
+		else if (IsRRCase(c, c2))
+		{
+			node = RotateLeft(x);
+		}
+		else if (IsLRCase(c, c2))
+		{
+			RotateRight(c);
+			node = RotateLeft(x);
+		}
+		else if (IsRLCase(c, c2))
+		{
+			RotateLeft(c);
+			node = RotateRight(x);
+		}
+
+		node->bf = GetNodeDepth(node->left) - GetNodeDepth(node->right);
+	}
+
+	return node->bf + (node != _root ? 1 : 0);
+}
+
+/// <summary>
+/// 주어진 노드의 깊이를 구하여 반환한다.
+/// </summary>
+/// <param name="node">시작 노드</param>
+/// <returns>시작 노드의 최대 깊이</returns>
+int AVLTree::GetNodeDepth(AVLNode* node, int depth)
+{
+	if (node == nullptr)
+	{
+		return depth;
+	}
+
+	int leftDepth{ GetNodeDepth(node->left, depth + 1) };
+	int rightDepth{ GetNodeDepth(node->right, depth + 1) };
+
+	return (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
+
+/// <summary>
+/// 주어진 노드의 배치가 LL케이스인지 여부를 반환한다.
+/// </summary>
+/// <param name="c">문제 노드의 자식</param>
+/// <param name="c2">c의 자식</param>
+/// <returns>LL케이스인지 여부</returns>
+bool AVLTree::IsLLCase(AVLNode* c, AVLNode* c2)
+{
+	if (IsLeftNode(c) && IsLeftNode(c2))
+	{
+		return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// 주어진 노드의 배치가 RR케이스인지 여부를 반환한다.
+/// </summary>
+/// <param name="c">문제 노드의 자식</param>
+/// <param name="c2">c의 자식</param>
+/// <returns>RR케이스인지 여부</returns>
+bool AVLTree::IsRRCase(AVLNode* c, AVLNode* c2)
+{
+	if (IsRightNode(c) && IsRightNode(c2))
+	{
+		return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// 주어진 노드의 배치가 LR케이스인지 여부를 반환한다.
+/// </summary>
+/// <param name="c">문제 노드의 자식</param>
+/// <param name="c2">c의 자식</param>
+/// <returns>LR케이스인지 여부</returns>
+bool AVLTree::IsLRCase(AVLNode* c, AVLNode* c2)
+{
+	if (IsLeftNode(c) && IsRightNode(c2))
+	{
+		return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// 주어진 노드의 배치가 RL케이스인지 여부를 반환한다.
+/// </summary>
+/// <param name="c">문제 노드의 자식</param>
+/// <param name="c2">c의 자식</param>
+/// <returns>RL케이스인지 여부</returns>
+bool AVLTree::IsRLCase(AVLNode* c, AVLNode* c2)
+{
+	if (IsRightNode(c) && IsLeftNode(c2))
+	{
+		return true;
+	}
+	return false;
 }
 #pragma endregion
--- 검색트리/AVLTree.h
+++ 검색트리/AVLTree.h
@@ -19,6 +19,11 @@ struct AVLNode
 		right = nullptr;
 	}
 
+	bool HasBalanceProblem()
+	{
+		return bf < -1 || 1 < bf;
+	}
+
 	int data;
 	int bf;
 	AVLNode* parent;
@@ -55,13 +60,25 @@ public:
 	void Delete(int data);
 
 private:
-	AVLNode* Insert(AVLNode* parent, int data);
-	AVLNode* Delete(AVLNode* node);
+	void Insert(AVLNode* parent, int data);
+	void Delete(AVLNode* node);
 
 	AVLNode* GetNode(int data);
 
 	bool IsLeftNode(AVLNode* node);
 	bool IsRightNode(AVLNode* node);
+
+	AVLNode* RotateLeft(AVLNode* node);
+	AVLNode* RotateRight(AVLNode* node);
+
+	int CalculateBalaceFactor(AVLNode* node);
+
+	int GetNodeDepth(AVLNode* node, int depth = 0);
+
+	bool IsLLCase(AVLNode* c, AVLNode* c2);
+	bool IsRRCase(AVLNode* c, AVLNode* c2);
+	bool IsLRCase(AVLNode* c, AVLNode* c2);
+	bool IsRLCase(AVLNode* c, AVLNode* c2);
 
 private:
 	AVLNode* _root;
