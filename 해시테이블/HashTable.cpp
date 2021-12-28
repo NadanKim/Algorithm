f987278529bfc531cd3e32079c5b50a95393fb8e
김나단 <paintime@naver.com>
2021-12-28 18:20:51 +0900

    HashTable 기본 클래스 구현

--- 해시테이블/HashTable.cpp
+++ 해시테이블/HashTable.cpp
@@ -1,9 +1,64 @@
 #include "HashTable.h"
 
+#pragma region Public Functions
 /// <summary>
 /// 해시 테이블의 현 상태를 출력한다.
 /// </summary>
 void HashTable::PrintHashTable()
 {
 	std::cout << "--- " << typeid(this).name() << " ---\n";
+	std::cout << "- HashFuncion : " << GetHashFunctionString() << "---\n";
 }
+#pragma endregion
+
+#pragma region Private Functions
+/// <summary>
+/// 나누기 방법을 이용해 주어진 값의 해쉬 인덱스를 생성해 반환한다.
+/// </summary>
+/// <param name="data">인덱스를 찾을 데이터</param>
+/// <returns>해쉬 테이블에서의 인덱스</returns>
+int HashTable::GetHashIndexByDivision(int data)
+{
+	return data % m_size;
+}
+
+/// <summary>
+/// 곱하기 방법을 이용해 주어진 값의 해쉬 인덱스를 생성해 반환한다.
+/// </summary>
+/// <param name="data">인덱스를 찾을 데이터</param>
+/// <returns>해쉬 테이블에서의 인덱스</returns>
+int HashTable::GetHashIndexByMultiplication(int data)
+{
+	static double multiplePrimeNumber = (std::sqrt(5) - 1.0) / 2.0;
+	return static_cast<int>(std::floor(std::fmod(data * multiplePrimeNumber, 1) * m_size));
+}
+
+/// <summary>
+/// 현재 해쉬 함수를 string 으로 반환한다.
+/// </summary>
+/// <returns>해쉬 함수 string</returns>
+std::string HashTable::GetHashFunctionString()
+{
+	if (m_hashFunction == HashFunction::Multiplication)
+	{
+		return "Multiplication";
+	}
+	return "Division";
+}
+#pragma endregion
+
+#pragma region Protected Functions
+/// <summary>
+/// 주어진 데이터를 이용하여 해쉬 인덱스를 생성해 반환한다.
+/// </summary>
+/// <param name="data">인덱스를 찾을 데이터</param>
+/// <returns>해쉬 테이블에서의 인덱스</returns>
+int HashTable::GetHashIndex(int data)
+{
+	if (m_hashFunction == HashFunction::Multiplication)
+	{
+		return GetHashIndexByMultiplication(data);
+	}
+	return GetHashIndexByDivision(data);
+}
+#pragma endregion
--- 해시테이블/HashTable.h
+++ 해시테이블/HashTable.h
@@ -1,5 +1,16 @@
 #pragma once
 #include "../Common.h"
+#include <cmath>
+#include <string>
+
+/// <summary>
+/// 해시 테이블에서 사용할 해시 함수
+/// </summary>
+enum class HashFunction
+{
+	Division,
+	Multiplication,
+};
 
 /// <summary>
 /// 해시 테이블의 구성 요소 테스트를 위한 베이스 클래스
@@ -7,6 +18,8 @@
 class HashTable
 {
 public:
+	HashTable(HashFunction hashFunction) : m_hashFunction(hashFunction), m_size(Default_Table_Size){};
+
 	virtual void Add(int data) = 0;
 	virtual void Remove(int data) = 0;
 	virtual bool Contains(int data) = 0;
@@ -14,6 +27,22 @@ public:
 
 	virtual void PrintHashTable();
 
+private:
+	int GetHashIndexByDivision(int data);
+	int GetHashIndexByMultiplication(int data);
+
+	std::string GetHashFunctionString();
+
 protected:
-	virtual int GetHashIndex(int data) = 0;
+	int GetHashIndex(int data);
+
+	int Size() { return m_size; }
+
+private:
+	HashFunction m_hashFunction;
+	int m_size;
+
+	static int Default_Table_Size;
 };
+
+int HashTable::Default_Table_Size = 17;
